50592549b00f5982f969ac093e172ba402a744d9
Hakim <barouni.hakim@gmail.com>
2021-03-24 17:59:30 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Counter from './components/Counter';
+import './App.css'
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+state={
+
+display:true
+
 }
 
-export default App;
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={()=> this.setState({display:!this.state.display})} >
+          {this.state.display ? "hide counter" : "show counter" }</button>
+
+       { this.state.display ? (
+        <Counter title="This is a counter"/>) : ""
+
+  }
+      </div>
+    )
+  }
+}
+
---
+++ src/components/Counter.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+
+constructor(props) {
+
+super(props);
+
+this.state={
+count : 0 ,
+message : "" 
+
+
+}
+
+
+}
+
+componentDidMount(){
+
+console.log('component is mounted')
+}
+
+componentDidUpdate() {
+
+console.log('component updated !')
+
+}
+
+componentWillUnmount(){
+    console.log('component is dead !');
+}
+
+
+
+increment =() => this.setState ({count : this.state.count +1}) ;
+
+decrement() { 
+    
+this.setState({count : this.state.count -1});
+
+
+}
+
+    render() {
+        return (
+            <div>
+
+                
+
+                
+                <h1>{this.props.title}</h1>
+                <button type="button" className="btn btn-success" style={{width:"50px" }} onClick={
+                
+                ()=> this.increment()
+
+
+
+                }>+</button>
+                <span style={{margin:"20px 30px"}}>{this.state.count}</span>
+                <button type="button" className="btn btn-danger" style={{width:"50px"}} onClick={
+
+                ()=> this.state.count>0 ? this.decrement() : alert('stop ! ')
+
+
+
+                } >-</button> <br/>
+
+               <input type="text"  onChange={(e) => {
+                   
+                this.setState({message : e.target.value }) ;
+                // console.log(e) ;
+            
+            
+            
+            
+            }}/>
+
+            <p>{this.state.message}</p>
+            </div>
+        )
+    }
+}
